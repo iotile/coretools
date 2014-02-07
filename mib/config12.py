6cdef56a25d3af9ac45236ecdaa94eb0d28bae24
Tim Burke <timburke@alumni.rice.edu>
2014-02-06 18:08:56 -0800

    Made mib api region a normal psect and link in the right location
    - change mib api from an absoluted located psect to a regular one
    with a linker flag to put it in the right spot.  This is cleaner,
    safer and better.  It also removes the ability of other psects
    to inadvertantly stomp over the api.
    - change the pic12 build system to pass the right linker flag
    to place the the mibapi psect in the right spot
    - add mibtool api command that dumps the mib api from a mib12
    executive hex file.
    - refactor chip specific addresses into pymomo from site_scons
    so that they can be shared with mibtool easily

---
+++ mib/config12.py
@@ -0,0 +1,25 @@
+#mib12_config.py
+
+class MIB12Processor:
+	"""
+	Collection of settings pertaining to the location of various important
+	ROM and RAM regions in the MIB12 framework.
+	"""
+
+
+	def __init__(self, name, settings):
+		self.settings = settings
+		self.name = name
+
+		self._calculate()
+
+	def _calculate(self):
+		self.exec_rom = self.settings['executive_rom']
+		self.app_rom = [self.exec_rom[1] +1, self.settings['total_rom']-1]
+
+		self.api_range = [self.app_rom[0] - 16, self.app_rom[0] - 1]
+		self.mib_range = [2048 - 16, 2047] #mib map is 16 bytes long
+
+		self.row_size = self.settings['flash_row_words']
+		self.total_prog_mem = self.settings['total_rom']
+		self.first_app_page = self.app_rom[0] / self.row_size
--- utilities/build.py
+++ utilities/build.py
@@ -13,3 +13,33 @@ def load_settings():
 		return json.load(f)
 
 	ValueError('Could not load global build settings file (config/build_settings.json)')
+
+def load_chip_info(chip):
+	"""
+	Load chip info from chip_settings dictionary in build_settings.json
+	"""
+	conf = load_settings()
+	settings = conf['mib12']['chip_settings'][chip]
+
+	aliases = []
+	if 'aliases' in settings:
+		aliases = settings['aliases']
+
+	default = conf['mib12']['default_settings'].copy()
+	chip_info = merge_dicts(default, settings)
+
+	return (aliases, chip_info)
+
+def merge_dicts(a, b):
+    "merges b into a"
+
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge_dicts(a[key], b[key])
+            else:
+            	a[key] = b[key]
+        else:
+            a[key] = b[key]
+    
+    return a
