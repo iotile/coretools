fcb8ec59cc0b61dacd718c4d1a874968b93cafb7
Tim Burke <timburke@alumni.rice.edu>
2014-03-10 23:06:25 -0700

    Add 16 bit hex file manipulation routines

---
+++ hex16/convert.py
@@ -0,0 +1,25 @@
+#convert.py
+#Convert PIC24 hex files into a form that can be programmed for recovery
+#Each pic24 instruction is 3 bytes.  XC16 pads this to 4 bytes.  We can't
+#have it padded since we need to program it into the device directly
+
+import intelhex as ih
+
+def unpad_pic24_hex(hexfile):
+	"""
+	Given a pic24 hex file, return an intel hex object that is not padded
+	"""
+
+	f = ih.IntelHex(hexfile)
+	out = ih.IntelHex()
+
+	i = 0
+	j = 0
+
+	for i in xrange(0, len(f), 4):
+		out[j+0] = f[i+0]
+		out[j+1] = f[i+1]
+		out[j+2] = f[i+2]
+		j += 3
+
+	return out
