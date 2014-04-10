896136d5dce8f218feb516d48df2e54c29809d05
Austin McGee <austinwm@gmail.com>
2014-04-10 15:13:06 -0700

    Fix momosensor tool to expect a 32-bit value.

--- commander/types/sensor_event.py
+++ commander/types/sensor_event.py
@@ -8,10 +8,10 @@ class SensorEvent:
 	"""
 
 	def __init__(self, buffer):
-		if len(buffer) != 16:
-			raise TypeException( 'SensorEvent', 'Length should have been 16, was %d' % len(buffer) )
+		if len(buffer) != 12:
+			raise TypeException( 'SensorEvent', 'Length should have been 12, was %d' % len(buffer) )
 
-		stream, metadata, timestamp_year, timestamp_month, timestamp_day, timestamp_hours, timestamp_minutes, timestamp_seconds, value = struct.unpack('BBBBBBBBQ', buffer)
+		stream, metadata, timestamp_year, timestamp_month, timestamp_day, timestamp_hours, timestamp_minutes, timestamp_seconds, value = struct.unpack('BBBBBBBBI', buffer)
 
 		if timestamp_year < 1960:
 			timestamp_year = 1960
