c82a8f8a9da94184c3087a342aff521a9be5facb
Tim Burke <timburke@alumni.rice.edu>
2014-05-27 20:23:49 -0700

    Modify reflash to wait for reflash to finish before sending reset.
    Requires that the new pic12_executive be loaded to work correctly.
    If the old executive is loaded, it will throw an error saying
    reflash not detected and quit.  The reflash is still working and
    after ~20 seconds (<30 seconds) for the pic16lf1847, the reflash
    will succeed.  This allows upgrading from an old pic12_executive
    using the new modtool.

--- commander/meta/reflash.py
+++ commander/meta/reflash.py
@@ -24,17 +24,20 @@ def reflash_module(controller, hexfile, name=None, address=None, force=False, ve
 	mod.rpc(1, 0, 8, bucket)
 	mod.reset()
 
-	if verbose:
-		prog = ProgressBar("Reflashing", 10)
-		prog.start()
+	sleep(1.5)
+	if not controller.alarm_asserted():
+		print "Module reflash NOT DETECTED.  Verify the module checksum to ensure it is programmed correctly."
+		raise RuntimeError("Could not reflash module, reflash not detected using alarm pin.")
 
-		for i in xrange(0, 10):
-			sleep(1)
-			prog.progress(i)
+	print "Reflash in progress"
+	while controller.alarm_asserted():
+		sys.stdout.write('.')
+		sys.stdout.flush()
+		sleep(0.1)
 
-		prog.end()
-	else:
-		sleep(10)
+	print "\nReflash complete."
+
+	sleep(0.5)
 
 	if not noreset: 
 		if verbose:
