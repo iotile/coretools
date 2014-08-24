d85d5c9f12c6cd6f8afa8ff154802a7241b8d148
Tim Burke <timburke@alumni.rice.edu>
2014-08-24 11:55:11 -0700

    Python unit tests fror pic24 build system
    Uses pyunit and nose for testing.  Current tests:
    - build_settings file exists and is valid
    - ChipFamily and TargetSettings objects work

---
+++ test/test_imports.py
@@ -0,0 +1,16 @@
+#basic tests to ensure that everything is installed and working properly
+
+def test_required_packages():
+	"""
+	Make sure that the required dependencies of pymomo are installed
+	"""
+
+	import decorator
+	#TODO add the rest of the packages
+
+def test_import_pymomo():
+	"""
+	Make sure pymomo is accessible for import
+	"""
+
+	import pymomo
