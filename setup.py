7e115249ecdd7f44081f1a894cb4cf00314c9a7f
Austin McGee <austinwm@gmail.com>
2015-01-21 13:19:43 -0800

    v1.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def list_data_files():
 setup(
     name = "pymomo",
     packages = find_packages(),
-    version = "1.0.2",
+    version = "1.0.3",
     install_requires=[
         "beautifulsoup4==4.3.2",
         "BTrees==4.1.1",
