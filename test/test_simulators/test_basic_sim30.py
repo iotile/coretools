233a0c287a9578eafcb9eae88204e0e3b413dd27
Austin McGee <austinwm@gmail.com>
2015-01-22 11:56:11 -0800

    Add Windows CI support using appveyor.com.
    TODO: pywin32 installation for shell support, Windows 64-bit testing

--- test/test_simulators/test_basic_sim30.py
+++ test/test_simulators/test_basic_sim30.py
@@ -4,23 +4,17 @@ from pymomo.sim import simulator
 from pymomo.utilities.config import ConfigFile
 from distutils.spawn import find_executable
 
+@unittest.skipIf(find_executable("sim30") == None, 'SIM30 executable not found')
 class TestSIM30BasicFunctionality(unittest.TestCase):
 	"""
 	Test to make sure the SIM30 pic24 simulator is working
 	correctly
 	"""
-
 	def setUp(self):
-		sim30path = find_executable("sim30")
-
-		if not os.path.exists(sim30path):
-			unittest.skip('SIM30 executable not found')
-		else:
-			self.sim = simulator.Simulator('pic24')
+		self.sim = simulator.Simulator('pic24')
 
 	def tearDown(self):
-		if hasattr(self, 'sim'):
-			self.sim.exit()
+		self.sim.exit()
 
 	def test_basic_functionality(self):
 		self.assertTrue(self.sim.ready())
