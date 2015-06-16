91aad7e74d06165bae9403da1b61b9b8157fd990
Tim Burke <timburke@alumni.rice.edu>
2015-06-16 09:18:55 -0700

    Update rpc to autoretry on module busy

--- pymomo/commander/proxy/proxy.py
+++ pymomo/commander/proxy/proxy.py
@@ -2,6 +2,7 @@
 
 from pymomo.commander.commands import RPCCommand
 from pymomo.commander.exceptions import *
+from time import sleep
 
 class MIBProxyObject (object):
 	def __init__(self, stream, address):
@@ -16,7 +17,6 @@ class MIBProxyObject (object):
 		is not successful.
 		"""
 
-
 		r = RPCCommand(self.addr, feature, cmd, *args)
 		self.stream.send_cmd(r)
 
@@ -27,8 +27,20 @@ class MIBProxyObject (object):
 
 		res = r.parse_result(*res_type)
 
+		#if the module is busy try to send the command again up to a maximum of 10 times
+		if res['is_error'] and res['status'] == 0:
+			if "retries" not in kw:
+				kw['retries'] = 10
+
+			#Sleep 100 ms and try again unless we've exhausted our retry attempts
+			if kw["retries"] > 0:
+				kw['retries'] -= 1
+
+				sleep(0.1)
+				return self.rpc(feature, cmd, *args, **kw)
+
 		if "raise" not in kw or kw['raise'] == True:
-			if res['status'] != 0 or res['error'] != 'No Error':
+			if res['is_error'] == True:
 				raise RPCException(res['status'], res['error'])
 
 		return res
