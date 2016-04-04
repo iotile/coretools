5ddef5573283fc198da004465ae691f01b502204
Tim Burke <timburke@alumni.rice.edu>
2016-04-03 20:09:50 -0700

    Update command_map template to include app info block

--- pymomo/config/templates/command_map_c.c
+++ pymomo/config/templates/command_map_c.c
@@ -1,6 +1,6 @@
-\#define NOEXTERNCOMMANDTABLE
+\#define NOEXTERNAPPINFO
 \#include "command_map_c.h"
-\#undef NOEXTERNCOMMANDTABLE
+\#undef NOEXTERNAPPINFO
 
 #for $num, $id in $enumerate($sorted($commands.keys()))
 uint8_t ${commands[$id].symbol}(uint8_t *buffer, unsigned int length, uint8_t *out_buffer, unsigned int *out_length);
@@ -16,4 +16,35 @@ const cdb_slave_entry cdb_command_map[kNumCDBCommands] =
 
 #end if
 #end for
+};
+
+const CDBApplicationInfoBlock __attribute__((section (".block.appinfo"))) app_info = {
+	//Hardware and API compatibility information
+	kModuleHardwareType,
+	kAPIMajorVersion,
+	kAPIMinorVersion,
+
+	//Module Name
+	kModuleName,
+
+	//Module version information
+	kModuleMajorVersion,
+	kModuleMinorVersion,
+	kModulePatchVersion,
+
+	kNumCDBCommands,
+
+	//Reserved bytes
+	0,
+	0,
+	0,
+	0,
+
+	cdb_command_map,
+
+	//Magic number for recognizing CDB block
+	kCDBMagicNumber,
+
+	//Reserved for firmware checksum image to be patched in
+	0
 };
--- pymomo/config/templates/command_map_c.h
+++ pymomo/config/templates/command_map_c.h
@@ -2,6 +2,7 @@
 \#define __command_map_c_h__
 
 \#include <stdint.h>
+\#include "cdb_application.h"
 
 \#define kAPIMajorVersion		$api_version[0]
 \#define kAPIMinorVersion		$api_version[1]
@@ -14,17 +15,8 @@
 
 \#define kNumCDBCommands		($len($commands))
 
-typedef uint8_t (*cdb_slave_handler)(uint8_t *buffer, unsigned int length, uint8_t *out_buffer, unsigned int *out_length);
-
-typedef struct
-{
-	cdb_slave_handler	handler;
-	uint16_t			command;
-	uint16_t			reserved;
-} cdb_slave_entry;
-
-\#ifndef NOEXTERNCOMMANDTABLE
-extern const cdb_slave_entry cdb_command_map[kNumCDBCommands];
+\#ifndef NOEXTERNAPPINFO
+extern const CDBApplicationInfoBlock app_info;
 \#endif
 
 \#endif
