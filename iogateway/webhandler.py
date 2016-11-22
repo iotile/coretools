ed4db75e457e779766b44bb8d235a932c1943927
Tim Burke <timburke@alumni.rice.edu>
2016-11-22 13:53:15 -0800

    Remove unnecessary files

--- iogateway/webhandler.py
+++
@@ -1,7 +0,0 @@
-import tornado.web
-
-class IndexHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self):
-        self.write("This is your response")
-        self.finish()
