b8f9f311475d05990a0072fccc0363dafe26e7ae
Tim Burke <timburke@alumni.rice.edu>
2016-12-08 16:34:30 -0800

    Add iotiletest into unit testing list

--- scripts/release.py
+++ scripts/release.py
@@ -23,8 +23,9 @@ import glob
 comp_names = {
     'iotilecore': ['iotile-core', 'iotilecore'],
     'iotilebuild': ['iotile-build', 'iotilebuild'],
+    'iotiletest': ['iotile-test', 'iotiletest'],
     'iotilegateway': ['iotile-gateway', 'iotilegateway'],
-    'iotile_transport_bled112': ['iotile-transport-bled112', 'transport_plugins/bled112'] 
+    'iotile_transport_bled112': ['iotile-transport-bled112', 'transport_plugins/bled112']
 }
 
 def send_slack_message(message):
