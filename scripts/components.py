264ae8fc46367c2c182656a34eb8865b051906a1
Tim Burke <timburke@alumni.rice.edu>
2016-12-12 11:18:52 -0800

    Add status command line program
    Can check whether components have unreleased features

---
+++ scripts/components.py
@@ -0,0 +1,7 @@
+comp_names = {
+    'iotilecore': ['iotile-core', 'iotilecore'],
+    'iotilebuild': ['iotile-build', 'iotilebuild'],
+    'iotiletest': ['iotile-test', 'iotiletest'],
+    'iotilegateway': ['iotile-gateway', 'iotilegateway'],
+    'iotile_transport_bled112': ['iotile-transport-bled112', 'transport_plugins/bled112']
+}
---
+++ scripts/status.py
@@ -0,0 +1,96 @@
+"""Check whether there are changes since the last released version of a component
+"""
+import subprocess
+import cmdln
+import sys
+import components
+
+def get_tags():
+    data = subprocess.check_output(['git', '--no-pager', 'tag'])
+
+    tags = data.split('\n')
+    tags = [x for x in tags if len(x) > 0]
+
+    releases = [(x.partition('-')[0], x.partition('-')[2]) for x in tags]
+    return releases
+
+def get_released_versions(component):
+    """Get all released versions of the given component ordered newest to oldest
+    """
+
+    releases = get_tags()
+
+    return sorted([x for x in releases if x[0] == component], key=lambda x: x[1])[::-1]
+
+def get_changed_since_tag(tag, filter_dir):
+    data = subprocess.check_output(['git', '--no-pager', 'diff', '--name-only', tag, '--', filter_dir])
+    return data.strip()
+
+class StatusProcessor(cmdln.Cmdln):
+    name = 'status'
+
+    def do_releases(self, subcmd, opts, component):
+        """${cmd_name}: print all releases for the given component
+        
+        ${cmd_usage}
+        ${cmd_option_list}
+        """
+
+        releases = get_released_versions(component)
+        for x in releases:
+            print("{} - {}".format(*x))
+
+    def do_latest(self, subcmd, opts, component):
+        """${cmd_name}: print the latest release version of a given component
+        
+        ${cmd_usage}
+        ${cmd_option_list}
+        """
+
+        releases = get_released_versions(component)
+        if len(releases) == 0:
+            print("NOT RELEASED")
+            return 1
+        else:
+            print(releases[0][1])
+            return 0
+
+    def do_dirty(self, subcmd, opts):
+        """${cmd_name}: check if any components have unreleased changes
+        
+        ${cmd_usage}
+        ${cmd_option_list}
+        """
+
+        for comp_name, comp_parts in components.comp_names.iteritems():
+            releases = get_released_versions(comp_name)
+            if len(releases) == 0:
+                print(comp_name + ' - ' + 'No tagged releases')
+            else:
+                latest_tag = '-'.join(releases[0])
+                data = get_changed_since_tag(latest_tag, comp_parts[1])
+                if len(data) > 0:
+                    print(comp_name + ' - ' + 'Changed files in component tree')
+
+    def do_changed(self, subcmd, opts, component):
+        """${cmd_name}: print all files changes in component since the latest release
+        
+        ${cmd_usage}
+        ${cmd_option_list}
+        """
+
+        releases = get_released_versions(component)
+        latest = releases[0]
+
+        filter_dir = components.comp_names[component][1]
+
+        latest_tag = '-'.join(latest)
+        data = get_changed_since_tag(latest_tag, filter_dir)
+
+        if len(data) > 0:
+            print(data)
+
+
+if __name__ == '__main__':
+    proc = StatusProcessor()
+    sys.exit(proc.main())
