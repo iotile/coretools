05e86b18e1ddb66cad9f969c3f9f3e13ffa46ec1
Tim Burke <timburke@alumni.rice.edu>
2017-05-11 13:56:35 -0700

    Remove typedargs and replace with separate package

--- iotilecore/iotile/core/utilities/typedargs/__init__.py
+++ iotilecore/iotile/core/utilities/typedargs/__init__.py
@@ -1,12 +1,37 @@
-# This file is adapted from python code released by WellDone International
-# under the terms of the LGPLv3.  WellDone International's contact information is
-# info@welldone.org
-# http://welldone.org
-#
-# Modifications to this file from the original created at WellDone International 
-# are copyright Arch Systems Inc.
+"""Glue package shim to keep imports working since typedargs was broken into new package."""
 
-#External API functions from this package
+import pkg_resources
 
-from annotate import param, returns, context, finalizer, takes_cmdline, annotated, return_type, stringable
-from typeinfo import type_system, iprint
+
+# Recreate all old imports
+from typedargs.annotate import param, returns, context, finalizer, takes_cmdline, annotated, return_type, stringable
+from typedargs.typeinfo import type_system, iprint
+
+def load_external_components():
+    """Load all external typed defined by iotile plugins.
+
+    This allows plugisn to register their own types for type annotations and
+    allows all registered iotile components that have associated type libraries to
+    add themselves to the global type system.
+    """
+
+    # Find all of the registered IOTile components and see if we need to add any type libraries for them
+    from iotile.core.dev.registry import ComponentRegistry
+
+    reg = ComponentRegistry()
+    modules = reg.list_components()
+
+    typelibs = reduce(lambda x, y: x+y, [reg.find_component(x).type_packages() for x in modules], [])
+    for lib in typelibs:
+        if lib.endswith('.py'):
+            lib = lib[:-2]
+
+        type_system.load_external_types(lib)
+
+    # Also search through installed distributions for type libraries
+    for entry in pkg_resources.iter_entry_points('iotile.type_package'):
+        mod = entry.load()
+        type_system.load_type_module(mod)
+
+
+load_external_components()
