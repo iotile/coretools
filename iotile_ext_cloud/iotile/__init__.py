1a0cb38b80700d1c14a3687730d6fb7fa11bf10d
lelithium <lcabrol@arch-iot.com>
2017-09-08 13:07:49 -0700

    wrong __init__ file

--- iotile_ext_cloud/iotile/__init__.py
+++ iotile_ext_cloud/iotile/__init__.py
@@ -1,6 +1,3 @@
 from pkgutil import extend_path
-from cloud.utilities import device_slug_to_id, device_id_to_slug
-from cloud.cloud import IOTileCloud
 
 __path__ = extend_path(__path__, __name__)
-__all__ = ['device_slug_to_id', 'device_id_to_slug', 'IOTileCloud']
---
+++ iotile_ext_cloud/iotile/cloud/__init__.py
@@ -0,0 +1,4 @@
+from .utilities import device_slug_to_id, device_id_to_slug
+from .cloud import IOTileCloud
+
+__all__ = ['device_slug_to_id', 'device_id_to_slug', 'IOTileCloud']
