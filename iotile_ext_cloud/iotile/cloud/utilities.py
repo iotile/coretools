4a7850090d7a372942e99e13b2433b6d88b1eb7e
lelithium <lcabrol@arch-iot.com>
2017-09-08 11:07:33 -0700

    new function to convert ids to slugs

--- iotile_ext_cloud/iotile/cloud/utilities.py
+++ iotile_ext_cloud/iotile/cloud/utilities.py
@@ -2,6 +2,8 @@
 
 from iotile.core.exceptions import ArgumentError
 
+from string import hexdigits
+
 def device_slug_to_id(slug):
     """Convert a d-- device slug to an integer.
 
@@ -28,3 +30,32 @@ def device_slug_to_id(slug):
         return int(short, 16)
     except ValueError as exc:
         raise ArgumentError("Invalid device slug with non-numeric components", error_mesage=str(exc), slug=slug)
+
+def device_slug_from_id(id):
+    """ Converts a device id into a correct device slug.
+
+    Args:
+        id (str or int) : A device id in either of the following formats :
+            XXXX XXXX XXXX XXXX
+            XXXXXXXXXXXXXXXX
+            0xXXXXXXXXXXXXXXXX
+    Returns:
+        str: The device slug in the d--XXXX-XXXX-XXXX-XXXX format
+    Raises:
+        ArgumentError: if the ID is more than 16 chars or if it contains characters outside 0123456789ABCDEF
+    """
+
+    id = str(id).replace(' ','') # get rid of the spaces
+
+    if (id.startswith('0x')):
+        id = id[2:] # remove 0x identifier
+
+    id = id.zfill(16).upper()   # pad to 16 chars and convert to uppercase
+    if (not set(id) <= set(hexdigits)):
+        raise ArgumentError("ID cannot contain non hexadecimal characters !")
+    if (len(id) > 16):
+        raise ArgumentError("ID cannot be more than 16 chars !")
+
+    chunks = [id[i:i+4] for i in range(0, len(id), 4)] # get 4 strings of 4 chars
+
+    return 'd--' + '-'.join(chunks)
