da113ac256161cbff2c74b3df9058cb104f3dff3
lelithium <lcabrol@arch-iot.com>
2017-09-08 11:18:05 -0700

    unit tests for device_id_to_slug

--- iotile_ext_cloud/test/test_utils.py
+++ iotile_ext_cloud/test/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 from iotile.core.exceptions import ArgumentError
-from iotile.cloud.utilities import device_slug_to_id
+from iotile.cloud.utilities import device_slug_to_id, device_id_to_slug
 
 
 def test_device_slug_to_id():
@@ -20,3 +20,16 @@ def test_device_slug_to_id():
 
     with pytest.raises(ArgumentError):
         device_slug_to_id(0x100)
+
+def test_device_id_to_slug():
+    assert device_id_to_slug('0x10') == 'd--0000-0000-0000-0010'
+    assert device_id_to_slug('1234 aBcD 5678 Ef90') == 'd--1234-abcd-5678-ef90'
+
+    with pytest.raises(ArgumentError):
+        device_id_to_slug('12345678901234567')
+
+    with pytest.raises(ArgumentError):
+        device_id_to_slug('non hexa chars')
+
+    with pytest.raises(ArgumentError):
+        device_slug_to_id(1234)
