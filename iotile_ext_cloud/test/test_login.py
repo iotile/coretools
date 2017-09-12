308c243bdfe3338d55bbb6fd4003333dd43eeabd
lelithium <lcabrol@arch-iot.com>
2017-09-11 17:40:56 -0700

    unit testing for get_whitelist and get_fleet functions

--- iotile_ext_cloud/test/test_login.py
+++ iotile_ext_cloud/test/test_login.py
@@ -6,7 +6,8 @@ from iotile.cloud.cloud import IOTileCloud
 from iotile.cloud.config import link_cloud
 from iotile.core.dev.config import ConfigManager
 from iotile.core.dev.registry import ComponentRegistry
-
+from iotile.core.exceptions import ArgumentError, ExternalError
+import json
 
 @pytest.fixture
 def registry():
@@ -111,4 +112,57 @@ def test_check_time():
         mocker.get('https://iotile.cloud/api/v1/server/', json=json_false)
         assert cloud.check_time() == False
 
+def test_get_fleet():
+    auth_payload = {
+        'jwt': 'big-token',
+        'username': 'user1'
+    }
+    test_payload = {"count":1,
+              "next":"Null",
+              "previous":"Null",
+              "results":[{"device":"d--0000-0000-0000-0001","always_on":True,"is_access_point":False}]}
+    manager = ConfigManager()
+    with requests_mock.Mocker() as mocker:
 
+        mocker.post('https://iotile.cloud/api/v1/auth/login/', json=auth_payload)
+        link_cloud(manager, 'user1@random.com', 'password')
+        cloud = IOTileCloud()
+        mocker.get('https://iotile.cloud/api/v1/fleet/g--0000-0000-0001/devices/', json=test_payload)
+        mocker.get('https://iotile.cloud/api/v1/fleet/g--0000-0000-0002/devices/', status_code=404)
+        assert cloud.get_fleet(1) == test_payload
+        with pytest.raises(ArgumentError):
+            cloud.get_fleet(2)
+        with pytest.raises(ArgumentError):
+            cloud.get_fleet(pow(16,12) + 1)
+
+def test_get_whitelist():
+    """ Make sure we can retrieve the whitelist correctly """
+    with open('test/large_mock_answer.json') as lma:
+        j = json.load(lma)
+        test_payload = j['whitelist_test']
+        p1 = j['whitelist_g1']
+        p2 = j['whitelist_g2']
+        p3 = j['whitelist_g3']
+        expected = j['expected']
+    payload = {
+        'jwt': 'big-token',
+        'username': 'user1'
+    }
+
+    manager = ConfigManager()
+    with requests_mock.Mocker() as mocker:
+
+        mocker.post('https://iotile.cloud/api/v1/auth/login/', json=payload)
+        link_cloud(manager, 'user1@random.com', 'password')
+        cloud = IOTileCloud()
+        mocker.get('https://iotile.cloud/api/v1/fleet/?device=d--0000-0000-0000-0001', status_code=404)
+        with pytest.raises(ExternalError):
+            cloud.get_whitelist(1)
+        mocker.get('https://iotile.cloud/api/v1/fleet/?device=d--0000-0000-0000-0002', json={'results':[]})
+        with pytest.raises(ExternalError):
+            cloud.get_whitelist(2)
+        mocker.get('https://iotile.cloud/api/v1/fleet/?device=d--0000-0000-0000-01bd', json=test_payload)
+        mocker.get('https://iotile.cloud/api/v1/fleet/g--0000-0000-0001/devices/', json=p1)
+        mocker.get('https://iotile.cloud/api/v1/fleet/g--0000-0000-0002/devices/', json=p2)
+        mocker.get('https://iotile.cloud/api/v1/fleet/g--0000-0000-0003/devices/', json=p3)
+        assert cloud.get_whitelist(0x1bd) == expected
