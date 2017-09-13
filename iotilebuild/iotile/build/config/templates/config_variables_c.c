7da9816581bad160e42f2028bf557e7bd2a5695f
x24ling <chris@arch-iot.com>
2017-09-13 15:04:33 -0700

    Removed extraneous comment in template

--- iotilebuild/iotile/build/config/templates/config_variables_c.c
+++ iotilebuild/iotile/build/config/templates/config_variables_c.c
@@ -25,7 +25,7 @@ $variable.type __attribute__((section(".required_config"))) $variable.name;
 config_${variable.name}_t __attribute__((section(".optional_config"))) $variable.name = {$variable.total_size, 0, #slurp
 #if type($variable.default_value)==str
 $variable.default_value}; #slurp
-#else                                  //Writing array in C format
+#else
 {#echo ','.join(str(x) for x in $variable.default_value)#}}; #slurp
 #end if
 
