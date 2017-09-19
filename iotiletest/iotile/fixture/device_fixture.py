93233db888e9f72eadce9dbbd80b4e4d958230f1
Tim Burke <timburke@alumni.rice.edu>
2017-09-18 17:13:48 -0700

    Make test fixtures more robust against noise
    Closes #272

--- iotiletest/iotile/fixture/device_fixture.py
+++ iotiletest/iotile/fixture/device_fixture.py
@@ -1,10 +1,12 @@
 import pytest
 from iotile.core.hw.hwmanager import HardwareManager
+from iotile.core.exceptions import HardwareError
 import time
 
 def pytest_addoption(parser):
     parser.addoption('--port', default="bled112", help="Port to use to connect to iotile device")
     parser.addoption('--uuid', action="append", default=[], help="Device UUIDs to run tests on")
+    parser.addoption('--tile', action="append", default=[], help="Device tile addresses to run tests on")
 
 def pytest_generate_tests(metafunc):
     if 'port' in metafunc.fixturenames:
@@ -16,6 +18,10 @@ def pytest_generate_tests(metafunc):
         ids = [int(x, 0) for x in metafunc.config.option.uuid]
         metafunc.parametrize("device_id", ids, scope='session')
 
+    if 'tile' in metafunc.fixturenames:
+        ids = [int(x, 0) for x in metafunc.config.option.tile]
+        metafunc.parametrize("tile", ids, scope='session')
+
 @pytest.fixture(scope='module')
 def device(port, device_id):
     """Return a HardwareManager instance connected to an IOTile Device
@@ -23,9 +29,16 @@ def device(port, device_id):
     This fixture shares the same device among all tests in the same module
     """
     with HardwareManager(port=port) as hw:
-        time.sleep(1)
-
-        hw.connect(device_id)
+        # Sometimes in congested wireless environments we can miss the
+        # device advertisement, so attempt the connection several times
+        # before giving up to improve test robustness.
+        max_attempts = 3
+        for _i in xrange(0, max_attempts):
+            try:
+                hw.connect(device_id)
+                break
+            except HardwareError:
+                time.sleep(1)
         hw.enable_streaming()
         yield hw
         hw.disconnect()
@@ -37,9 +50,14 @@ def per_test_device(port, device_id):
     This fixture creates and tears down the connection for each test
     """
     with HardwareManager(port=port) as hw:
-        time.sleep(1)
+        max_attempts = 3
+        for _i in xrange(0, max_attempts):
+            try:
+                hw.connect(device_id)
+                break
+            except HardwareError:
+                time.sleep(1)
 
-        hw.connect(device_id)
         hw.enable_streaming()
         yield hw
         hw.disconnect()
