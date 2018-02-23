e9dc520f3cb55e3a3d27f1d85466ddbc4bf545ba
x24ling <chris@arch-iot.com>
2018-02-23 10:44:39 -0800

    Better tests and checks, docstring, cp errors

--- iotilecore/iotile/core/hw/debug/flash_board_step.py
+++ iotilecore/iotile/core/hw/debug/flash_board_step.py
@@ -4,13 +4,22 @@ from iotile.core.hw.hwmanager import HardwareManager
 from iotile.core.exceptions import ArgumentError
 
 class FlashBoardStep (object):
+    """A Recipe Step used to flash firmware directly
+
+    Currentlly only supports using jlink to flash. Meant to bootstrap firmware
+
+    Args:
+        addresses (list[int]): List of slot addresses to check against
+        names     (list[str]): List of tile names to check against
+        versions  (list[str]): List of tile versions to check against.
+    """
     def __init__(self, args):
         if(args.get('file') is None):
-            raise ArgumentError("PromptStep Parameter Missing", parameter_name='file')
+            raise ArgumentError("FlashBoardStep Parameter Missing", parameter_name='file')
         if(args.get('port') is None):
-            raise ArgumentError("PromptStep Parameter Missing", parameter_name='port')
+            raise ArgumentError("FlashBoardStep Parameter Missing", parameter_name='port')
 
-        if 'jlink' not in args['port'][0:5]:
+        if not args['port'].startswith('jlink'):
             raise ArgumentError("FlashBoardStep is currently only possible through jlink", invalid_port = args['port'])
 
         self._file = args['file']
