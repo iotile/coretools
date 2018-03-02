0db1355d5712d2ab82198c96cab552344c3dcf0d
david Karchmer <dkarchmer@gmail.com>
2018-03-02 12:30:30 -0800

    Reuse IOTileDeviceSlug from iotile_cloud
    Main extra feature (apart from re-use) is that the device_id_to_slug
    now supports portions of slugs as well: d—0123 or 5abc on top of the
    integer.
    
    May be handy as we begin to write Apps that interact with users that
    may be think of the device ID as the one written on the box.
    
    Ideally, we switch iotilecore overall to take either slugs or ids, so I
    can also do `hw connect d—0123`

--- iotile_ext_cloud/iotile/cloud/utilities.py
+++ iotile_ext_cloud/iotile/cloud/utilities.py
@@ -1,6 +1,8 @@
 """Common utility functions used across iotile.cloud packages."""
 
+from iotile_clout.utils.gid import IOTileDeviceSlug
 from iotile.core.exceptions import ArgumentError
+
 
 def device_slug_to_id(slug):
     """Convert a d-- device slug to an integer.
@@ -18,38 +20,29 @@ def device_slug_to_id(slug):
     if not isinstance(slug, (str, unicode)):
         raise ArgumentError("Invalid device slug that is not a string", slug=slug)
 
-    if not slug.startswith("d--"):
-        raise ArgumentError("Invalid device slug without d-- prefix", slug=slug)
-
-    short = slug[3:]
-    short = short.replace('-', '')
-
     try:
-        return int(short, 16)
-    except ValueError as exc:
-        raise ArgumentError("Invalid device slug with non-numeric components", error_mesage=str(exc), slug=slug)
+        device_slug = IOTileDeviceSlug(id)
+    except Exception:
+        raise ArgumentError("Unable to recognize {} as a device id".format(id))
+
+    return device_slug.id
+
 
 def device_id_to_slug(id):
     """ Converts a device id into a correct device slug.
 
     Args:
         id (long) : A device id
+        id (string) : A device slug in the form of XXXX, XXXX-XXXX-XXXX-XXXX, d--XXXX, d--XXXX-XXXX-XXXX-XXXX
     Returns:
         str: The device slug in the d--XXXX-XXXX-XXXX-XXXX format
     Raises:
-        ArgumentError: if the ID is not in the [1, 16**16] range, or if it is not an int
+        ArgumentError: if the ID is not in the [1, 16**16] range, or if not a valid string
     """
-    if isinstance(id,int):
-        id = long(id)
-    elif not isinstance(id,long):
-        raise ArgumentError("Id is not a number")
-    if (id <= 0 or id > pow(16,16)):
-        raise ArgumentError("Id not in the correct range")
 
-    id = hex(id)[2:-1] # get rid of the 0x and the trailing L
+    try:
+        device_slug = IOTileDeviceSlug(id)
+    except Exception:
+        raise ArgumentError("Unable to recognize {} as a device id".format(id))
 
-    id = id.zfill(16).lower()   # pad to 16 chars and convert to lowercase
-
-    chunks = [id[i:i+4] for i in range(0, len(id), 4)] # get 4 strings of 4 chars
-
-    return 'd--' + '-'.join(chunks)
+    return str(device_slug)
