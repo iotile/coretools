f9189b8f1e0d2e965ba1eaf41aaf3c7e9af3cf95
x24ling <chris@arch-iot.com>
2018-03-01 17:07:21 -0800

    Addings tests and iotile-ship to coretools test

--- scripts/components.py
+++ scripts/components.py
@@ -4,6 +4,7 @@ comp_names = {
     'iotiletest': ['iotile-test', 'iotiletest'],
     'iotilegateway': ['iotile-gateway', 'iotilegateway'],
     'iotilesensorgraph': ['iotile-sensorgraph', 'iotilesensorgraph'],
+    'iotileship' : ['iotile-ship', 'iotileship'],
     'iotile_transport_bled112': ['iotile-transport-bled112', 'transport_plugins/bled112'],
     'iotile_transport_awsiot': ['iotile-transport-awsiot', 'transport_plugins/awsiot'],
     'iotile_transport_jlink': ['iotile-transport-jlink', 'transport_plugins/jlink'],
