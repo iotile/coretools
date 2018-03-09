25d84170fefc76e3e87098b8d58bc6795883ab59
Tim Burke <timburke@alumni.rice.edu>
2018-03-09 09:53:16 -0800

    Add test coverage of all current output formats

--- iotilesensorgraph/test/test_integration/test_output_formats.py
+++ iotilesensorgraph/test/test_integration/test_output_formats.py
@@ -13,6 +13,17 @@ def compare_binary_data(data, reference_file):
     assert data == bytearray(ref_data)
 
 
+def compare_string_data(data, reference_file):
+    """Compare utf-8 data with a reference file."""
+
+    in_path = os.path.join(os.path.dirname(__file__), 'reference_output', reference_file)
+
+    with open(in_path, "rb") as in_file:
+        ref_data = in_file.read()
+
+    assert data == ref_data.decode('utf-8')
+
+
 def test_script_output_format(usertick_gate_opt):
     """Make sure the output script format works."""
 
@@ -27,15 +38,17 @@ def test_snippet_output_format(usertick_gate_opt):
     """Make sure the snippet output format works."""
 
     snippet = format_snippet(usertick_gate_opt)
-
+    compare_string_data(snippet, 'usertick_optimized_snippet.txt')
 
 def test_config_output_format(usertick_gate_opt):
     """Make sure the config output format works."""
 
     config = format_config(usertick_gate_opt)
+    compare_string_data(config, 'usertick_optimized_config.txt')
 
 
 def test_ascii_output_format(usertick_gate_opt):
     """Make sure the ascii output format works."""
 
-    config = format_ascii(usertick_gate_opt)
+    ascii_txt = format_ascii(usertick_gate_opt)
+    compare_string_data(ascii_txt, 'usertick_optimized_ascii.txt')
