308214d67ba1b0e82c1db1072b219f3e718ff551
Tim Burke <timburke@alumni.rice.edu>
2018-04-13 12:17:22 -0700

    Fix unit test generation for qemu.
    - Properly copy over headers and referenced C modules

--- iotilebuild/test/test_iotilebuild/comp_with_tests/firmware/test/test_utilities.c
+++ iotilebuild/test/test_iotilebuild/comp_with_tests/firmware/test/test_utilities.c
@@ -1,7 +1,13 @@
 //name: test_utilities
 //type: qemu_semihost_unit
+//module: utilities.c
+
+#include "utilities.h"
+#include "unity.h"
 
 void test_utilities()
 {
+	int result = add(1, 2);
 
+	TEST_ASSERT(result == 3)
 }
