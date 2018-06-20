9fac0316b83b1a21bcbf9502d8294bf860bb47b4
x24ling <chris@arch-iot.com>
2018-06-20 14:48:42 -0700

    add tests, pylibftdi, fix conn_string/debug fixes

--- transport_plugins/jlink/setup.py
+++ transport_plugins/jlink/setup.py
@@ -10,7 +10,8 @@ setup(
     license="LGPLv3",
     install_requires=[
         "iotile-core>=3.6.2",
-        "pylink-square>=0.0.10"
+        "pylink-square>=0.0.10",
+        "pylibftdi>=0.17.0"
     ],
 
     entry_points={'iotile.device_adapter': ['jlink = iotile_transport_jlink.jlink:JLinkAdapter']},
