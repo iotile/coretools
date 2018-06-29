e8d6e18f0ab10e9943077787729bd816cbfa065c
x24ling <chris@arch-iot.com>
2018-06-29 15:18:03 -0700

    FlashBoardStep uses shared resources

--- iotilecore/iotile/core/hw/debug/flash_board_step.py
+++ iotilecore/iotile/core/hw/debug/flash_board_step.py
@@ -5,30 +5,36 @@ from iotile.core.exceptions import ArgumentError
 
 class FlashBoardStep (object):
     """A Recipe Step used to flash firmware directly
+    
+    Currently only supports using jlink to flash. Used to bootstrap firmware
 
-    Currently only supports using jlink to flash. Meant to bootstrap firmware
+    This function requires a shared hardware manager resource to be setup
+    containing a connected device that we can send the script to.
 
     Args:
         file (str): Firmware file name to flash
-        port (str): Port used. Currently only allows jlink port connections.
         debug_string (str): Optional connection string for debug
     """
+    REQUIRED_RESOURCES = [('connection', 'hardware_manager')]
     FILES = ['file']
 
     def __init__(self, args):
         if 'file' not in args:
             raise ArgumentError("FlashBoardStep required parameters missing", required=["file"], args=args)
-        if 'port' not in args:
-            raise ArgumentError("FlashBoardStep Parameter Missing", parameter_name='port', args=args)
-
-        if not args['port'].startswith('jlink'):
-            raise ArgumentError("FlashBoardStep is currently only possible through jlink", invalid_port=args['port'])
 
         self._file = args['file']
-        self._port = args['port']
         self._debug_string = args.get('debug_string')
 
-    def run(self):
-        with HardwareManager(port=self._port) as hw:
-            debug = hw.debug(self._debug_string)
-            debug.flash(self._file)
+    def run(self, resources):
+        """Runs the flash step
+
+        Args:
+            resources (dict): A dictionary containing the required resources that
+                we needed access to in order to perform this step.
+        """
+        if not resources['connection']._port.startswith('jlink'):
+            raise ArgumentError("FlashBoardStep is currently only possible through jlink", invalid_port=args['port'])
+
+        hwman = resources['connection']
+        debug = hwman.hwman.debug(self._debug_string)
+        debug.flash(self._file)
