9f96f6c6cc30069c19e75ed7f64c2dee35f3d9ce
Tim Burke <timburke@alumni.rice.edu>
2018-10-31 12:07:55 -0700

    Pin pyparsing version.

--- iotilesensorgraph/setup.py
+++ iotilesensorgraph/setup.py
@@ -23,7 +23,7 @@ setup(
     license="LGPLv3",
     description="IOTile SensorGraph Management and Simulation Package",
     install_requires=[
-        "pyparsing>=2.2.0",
+        "pyparsing~=2.2.0",
         "future>=0.16.0",
         "monotonic>=1.3.0",
         "toposort>=1.5",
