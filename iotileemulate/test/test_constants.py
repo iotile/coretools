0a62033c944870287a8b28218db660c165497403
Tim Burke <tim@arch-iot.com>
2018-11-06 21:10:24 -0800

    Update iotileemulate/test/test_constants.py

--- iotileemulate/test/test_constants.py
+++ iotileemulate/test/test_constants.py
@@ -1,7 +1,7 @@
 from iotile.emulate.constants import stream_name, rpc_name
 
 
-def test_stream_name_foramatting():
+def test_stream_name_formatting():
     """Make sure stream names are formatted correctly."""
 
     assert stream_name(0x5800 + 1024) == 'SYSTEM_RESET (0x5C00)'
