430e3abba8759bf589bb69c81d0e054d976bef3f
vvynohradov <41442348+vvynohradov@users.noreply.github.com>
2019-02-14 06:11:14 +0200

    Fix bug with sharing extensions between ComponentRegistry objects (#681)
    * Fix bug with sharing extensions between ComponentRegistry objects

--- iotilecore/iotile/core/dev/registry.py
+++ iotilecore/iotile/core/dev/registry.py
@@ -209,7 +209,7 @@ class ComponentRegistry(object):
         """Clear all previously registered extensions."""
 
         if group is None:
-            self._registered_extensions = {}
+            ComponentRegistry._registered_extensions = {}
             return
 
         if group in self._registered_extensions:
