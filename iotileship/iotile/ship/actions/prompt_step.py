2e1588855c468eafc78a33fa514336cc91cba785
Matt Runchey <matthew.runchey@gmail.com>
2019-10-23 11:39:56 -0700

    Fix the raw_input call in PromptStep for py3 (closes #883) Fix exception cleanup in recipe (closes #888)

--- iotileship/iotile/ship/actions/prompt_step.py
+++ iotileship/iotile/ship/actions/prompt_step.py
@@ -1,7 +1,8 @@
+"""PromptStep. Currently used to stall the recipe prior to running"""
 from iotile.ship.exceptions import RecipeActionMissingParameter
 
 
-class PromptStep:
+class PromptStep:  # pylint:disable=too-few-public-methods; all Steps have one "run" function
     """A Recipe Step used to prompt the user for input.
 
     Currently, this step doesn't store any information typed into
@@ -13,10 +14,10 @@ class PromptStep:
     """
     def __init__(self, args):
         if args.get('message') is None:
-            raise RecipeActionMissingParameter("PromptStep Parameter Missing", \
-                parameter_name='message')
+            raise RecipeActionMissingParameter("PromptStep Parameter Missing", parameter_name='message')
 
         self._message = args['message']
 
     def run(self):
-        raw_input(str(self._message))
+        """Get user input to exit the pause"""
+        input(str(self._message))
