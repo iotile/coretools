40755e1173bb2a9faf1e73db0312bfa49696e1f4
Tim Froehlich <tfroehlich@archsys.io>
2020-04-22 14:52:09 -0700

    Some cleanup

--- transport_plugins/bled112/iotile_transport_bled112/async_packet.py
+++ transport_plugins/bled112/iotile_transport_bled112/async_packet.py
@@ -56,10 +56,13 @@ class AsyncPacketBuffer:
 #23 16 00 00 a0 01 40 10 00 00
 #00 00 00 00 00 00  rssi: -54, type: 0, sender: 0090298125cb
 def packet_is_broadcast_v2(packet):
+    #Broadcast packets consist of 32 bytes for data, 10 for BLE packet header and 4 for bled112 bgapi header
     if len(packet) != 46:
         return False
+    #This identifies the bgapi packet as an event
     if not (packet[0] == 0x80 and packet[2] == 6 and packet[3] == 0):
         return False
+    #This identifies the event as a broadcast v2 packet
     if not (packet[18] == 0x1b and packet[19] == 0x16 and packet[20] == 0xdd and packet[21] == 0xfd):
         return False
     return True
@@ -67,24 +70,21 @@ def packet_is_broadcast_v2(packet):
 def packet_is_dupe(packet, dedupe_dict):
     mac = tuple(packet[6:11])
 
-    #Create the tuple for the packet, ignore the sequence value
-    broadcast = packet[22:]
-    del broadcast[13]
-    broadcast_tup = tuple(broadcast)
+    #Drop the sequence value from the packet, since that changes for each packet
+    data = packet[22:]
 
-    #print(f"Parsing from {mac}: {broadcast_tup}")
-    if dedupe_dict.get(mac, None) == broadcast_tup:
-        #print(f"Dropping from {mac}: {broadcast_tup}")
+    #print(f"Parsing from {mac}: {data}")
+    if dedupe_dict.get(mac, None) == data:
+        #print(f"Dropping from {mac}: {data}")
         return True
-    #print(f"Not a dupe    {mac}: {broadcast_tup}")
-    dedupe_dict[mac] = broadcast_tup
+    #print(f"Not a dupe    {mac}: {data}")
+    dedupe_dict[mac] = data
     return False
 
 
 def ReaderThread(filelike, read_queue, header_length, length_function, stop):
     logger = logging.getLogger(__name__)
     dedupe_dict = {}
-    #count = 0
 
     while not stop.is_set():
         try:
@@ -115,9 +115,8 @@ def ReaderThread(filelike, read_queue, header_length, length_function, stop):
             packet = header + remaining
 
             if packet_is_broadcast_v2(packet):
-                #count += 1
-                #if count % 100 != 0:
                 #if read_queue.qsize() > 25:
+                #if packet_is_dupe(packet, dedupe_dict):
                 if read_queue.qsize() > 25 and packet_is_dupe(packet, dedupe_dict):
                     #print("drop")
                     continue
