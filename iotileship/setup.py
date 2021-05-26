64c4030d1b21f8da8de5c25cb75896aa4c564673
Alexey Kuzmenko <dma@archsys.io>
2021-05-26 16:43:09 -0700

    Clean up iotileship false SCons messages

--- iotileship/setup.py
+++ iotileship/setup.py
@@ -71,8 +71,5 @@ IOTileship
 -----------
 
 A python package for shiping embedded firmware on IOTile based devices.  See https://www.arch-iot.com.
-
-IOTileship embeds SCons in accordance with its license permitting redistribution.  More information on
-SCons can be found at: https://scons.org
 """
 )
